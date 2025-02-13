ff24ff19580acfd721d75817d6d3384e30e4bc00
Francesco  Raguseo <francescoraguseo@Francescos-Air.broadband>
2025-02-13 10:21:27 +0000

    Update port to 10000

--- backend/server.js
+++ backend/server.js
@@ -49,5 +49,5 @@ app.post("/api/analyze", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
