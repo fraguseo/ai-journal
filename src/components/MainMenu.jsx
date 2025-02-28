75f2efb8036a4940e261416097387f0c40521236
Francesco  Raguseo <francescoraguseo@Francescos-MacBook-Air.local>
2025-02-28 13:43:27 +0000

    Add MainMenu component

---
+++ src/components/MainMenu.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  VStack,
+  Button,
+  Container,
+  Text,
+  Box,
+} from '@chakra-ui/react';
+import { 
+  EditIcon, 
+  ChatIcon, 
+  SpinnerIcon 
+} from '@chakra-ui/icons';
+
+function MainMenu({ onDiaryClick, onChatClick, onRecipesClick }) {
+  return (
+    <Container maxW="container.md" py={8}>
+      <VStack spacing={8}>
+        <Text fontSize="3xl" fontWeight="bold">
+          AI Journal
+        </Text>
+
+        <VStack spacing={4} w="100%">
+          <Button
+            leftIcon={<EditIcon />}
+            onClick={onDiaryClick}
+            size="lg"
+            w="100%"
+            colorScheme="teal"
+          >
+            Daily Diary
+          </Button>
+
+          <Button
+            leftIcon={<ChatIcon />}
+            onClick={onChatClick}
+            size="lg"
+            w="100%"
+            colorScheme="blue"
+          >
+            AI Chat
+          </Button>
+
+          <Button
+            leftIcon={<SpinnerIcon />}
+            onClick={onRecipesClick}
+            size="lg"
+            w="100%"
+            colorScheme="purple"
+          >
+            Mood Recipes
+          </Button>
+        </VStack>
+      </VStack>
+    </Container>
+  );
+}
+
+export default MainMenu; 
