f5c993b6051db55da756616d1b51f0c2f24572b7
Francesco  Raguseo <francescoraguseo@Francescos-Air.broadband>
2025-03-09 14:12:36 +0000

    Restore original button colors and icons in MainMenu

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -31,28 +31,65 @@ function MainMenu({
         <Heading>AI Journal</Heading>
         <Box w="100%">
           <VStack spacing={4}>
-            <Button colorScheme="blue" width="100%" onClick={onDiaryClick}>
-              Diary
+            <Button
+              leftIcon={<EditIcon />}
+              colorScheme="green"
+              width="100%"
+              onClick={onDiaryClick}
+              size="lg"
+            >
+              Daily Diary
             </Button>
-            <Button colorScheme="blue" width="100%" onClick={onChatClick}>
+            <Button
+              leftIcon={<ChatIcon />}
+              colorScheme="blue"
+              width="100%"
+              onClick={onChatClick}
+              size="lg"
+            >
               AI Chat
             </Button>
-            <Button colorScheme="blue" width="100%" onClick={onRecipesClick}>
+            <Button
+              leftIcon={<SpinnerIcon />}
+              colorScheme="yellow"
+              width="100%"
+              onClick={onRecipesClick}
+              size="lg"
+            >
               Mood Recipes
             </Button>
-            <Button colorScheme="blue" width="100%" onClick={onDreamClick}>
+            <Button
+              leftIcon={<MoonIcon />}
+              colorScheme="teal"
+              width="100%"
+              onClick={onDreamClick}
+              size="lg"
+            >
               Dream Journal
             </Button>
-            <Button colorScheme="blue" width="100%" onClick={onGoalsClick}>
+            <Button
+              leftIcon={<StarIcon />}
+              colorScheme="purple"
+              width="100%"
+              onClick={onGoalsClick}
+              size="lg"
+            >
               Goal Tracker
             </Button>
-            <Button colorScheme="blue" width="100%" onClick={onMorningThoughtsClick}>
+            <Button
+              leftIcon={<SunIcon />}
+              colorScheme="orange"
+              width="100%"
+              onClick={onMorningThoughtsClick}
+              size="lg"
+            >
               Morning Thoughts
             </Button>
             <Button
               colorScheme="red"
               width="100%"
               onClick={onLogout}
+              size="lg"
             >
               Sign Out
             </Button>
